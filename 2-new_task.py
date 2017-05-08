1957f8a6de724b99ff5dd79add4d71e094ef0ce3
Ravi Kumar <RAK@esi-group.com>
2017-05-08 20:11:58 +0530

    tutorials send recieve, persistance task queues

---
+++ 2-new_task.py
@@ -0,0 +1,20 @@
+import sys
+import pika
+
+message = ''.join(sys.argv[1:]) or "Hello World!"
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+
+channel = connection.channel()
+channel.queue_declare(queue='task_queue', durable=True)
+
+channel.basic_publish(exchange='',
+                      routing_key='task_queue',
+                      body=message,
+                      properties=pika.BasicProperties(
+                         delivery_mode = 2, # make message persistent
+                      ))
+
+print(" [x] Sent %r" % message)
+
+
+
